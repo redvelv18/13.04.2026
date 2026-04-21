67710fd15a94fab6b9b3391486bfc969d68fb470
Diego Sterling <diegosterling@Opti-03.localdomain>
2026-04-21 13:33:12 +0300

    Added orders in index

--- public/index.php
+++ public/index.php
@@ -4,11 +4,15 @@ ob_start();
 echo("<h1> Veikals </h1>");
 require(__DIR__ . '/../db/connect.php');
 require_once __DIR__ . '/../src/controllers/CustomerController.php';
+require_once __DIR__ . '/../src/controllers/OrderController.php';
 
 $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $requestUri = rtrim($requestUri, '/');
 
-if ($requestUri === '/customers') {
+if (preg_match('#^/clients/(\d+)/orders$#', $requestUri, $matches)) {
+    $_GET['id'] = $matches[1];
+    OrderController::index(); 
+} elseif ($requestUri === '/clients') {
     CustomerController::index();
 } else {
     http_response_code(404);
